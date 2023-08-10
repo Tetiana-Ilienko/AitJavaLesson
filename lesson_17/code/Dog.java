57d3e79cfabb0c437a8a17cf10e5fdbc899a6bc6
Tetiana Ilienko <tatyanailyenko@gmail.com>
2023-08-10 19:21:09 +0200

    change lesson 17 + Homework

--- lesson_17/code/Dog.java
+++ lesson_17/code/Dog.java
@@ -10,16 +10,19 @@ public class Dog {
     String name;
     int jumpHeight;
     int maxJump;
+    /*
+    Пустой конструктор создается по умолчанию. При создании своего конструктора, пустой исчезает - его надо
+    прописать вручную(если он нужен)
+     */
 
     public Dog(String name, int jumpHeight) { // Конструктор. Вызовется первым для того, чтобы объект мог существовать
         this.name = name;
         this.jumpHeight = jumpHeight;
         this.maxJump = jumpHeight * 2;
-
     }
 
-    void whoAmI() {
-        System.out.println("I am  " + name + ". I can jump  " + jumpHeight + " sm");
+    void whoAmI() {                          // метод
+        System.out.println("I am  " + name + ". I can jump  " + jumpHeight + " sm\n");
 
     }
 
@@ -32,6 +35,7 @@ public class Dog {
         System.out.println("I can jump " + height);
     }
 
+    //----------------------------------------------------------------------------
 //    void getBarrier(int barrierHeight) {
 //        if (barrierHeight <= maxJump && jumpHeight < barrierHeight) {
 //            while (jumpHeight <= barrierHeight) {
@@ -46,38 +50,51 @@ public class Dog {
 //            System.out.println("I can not jump  " + barrierHeight);
 //        }
 //    }
-    void getBarrier(int barrierHeight) {
-        System.out.println("Ap! Barrier!  " + barrierHeight );
+//    -------------------------------------------------------------------------------------
+    boolean getBarrier(int barrierHeight) {                      // метод возвращающий boolean переменную
+        System.out.println("Ap! Barrier!  " + barrierHeight);
+
+        // 84<=106 &&                     83 < 84
         if (barrierHeight <= maxJump && jumpHeight < barrierHeight) {
             while (jumpHeight <= barrierHeight) {
                 System.out.println("I have to training!");
                 training();
             }
         }
+        boolean result;
         if (jumpHeight >= barrierHeight) {
             jump(barrierHeight);
-            //System.out.println("I can jump");
+            result = true;
         } else {
-
-            System.out.println("I can not jump  " + barrierHeight);
+            System.out.println("I can not jump barrier  " + barrierHeight);
+            result = false;
         }
+        return result;
     }
 
 
     // правильный вариант
     void training() {
-        System.out.println("\nStart training:");
+        System.out.println("\nStart training:"); // есть собака с тек.прыжок 53, 106 -> барьер -> 105 (бесконечный цикл)
+        //2. jump 50 -> maxJump 100 ->
+        // jump [0..90] -> if
         if (jumpHeight + 10 <= maxJump) {     // если прыжок меньше мин на 10 от максимально - можем тренироваться
             jumpHeight += 10;
             System.out.println("I have been training");
             whoAmI();
-
+        }
+        //-> [91...infinity]
+        // [9    100 - [91..inf] < 10 ==> будет true при значения [91-99]
+        else if (maxJump - jumpHeight < 10) {
+            jumpHeight  = maxJump;
+            System.out.println("Small training start");
+            whoAmI();
         } else {
             System.out.println("I can't jump any higher than  " + maxJump); // иначе - прыгаем на максимум
         }
-        System.out.println("End training. Jumping " + jumpHeight);
+        System.out.println("End training. Jumping " + jumpHeight + "\n");
     }
-
+//-----------------------------------------------------------------------------------------
 //    void training() {
 //        System.out.println(" Start training");
 //        while (jumpHeight< maxJump){
@@ -86,7 +103,5 @@ public class Dog {
 //        }
 //        System.out.println("I can't jump any higher than" + maxJump);
 //    }
-
-
 }
 
--- lesson_17/code/HomeWork_17.java
+++ lesson_17/code/HomeWork_17.java
@@ -4,28 +4,37 @@ import java.util.Random;
 public class HomeWork_17 {
     public static void main(String[] args) {
 
-        Dog dog = new Dog("Mars", 60);
+        Dog dog = new Dog("Mars", 53);
        // dog.whoAmI();
-        int lengtArray = 5;
+        int lengtArray = 10;
         int range = 150;
         int[] barrierArray = makeArray(lengtArray, range);
         System.out.println("Barriers array  " + Arrays.toString(barrierArray));
         dog.whoAmI();
-        for (int barrier : barrierArray) {
-           // System.out.println("Ap! Barrier!  " + barrier );
-            dog.getBarrier(barrier);
-        }
+        int sucBarrierForDog = startTrainingForOneDog(dog,barrierArray);
+        System.out.println("Количество перепрыгнутых барьеров: " + sucBarrierForDog);
 
 
 
     }
 
+    public static int startTrainingForOneDog(Dog dog,int[] barriers){ // метод считающий взятые барьеры
+        int counter =0;
+        for (int barrier : barriers) {
+            System.out.println("Попытка перепрыгнуть барьер " + barrier);
+           if (dog.getBarrier(barrier)){
+               counter++;
+           }
+            System.out.println("End attempt \n");
+        }
+        return counter;
+    }
 
-    public static int[] makeArray(int lengtArray, int range) {
+    public static int[] makeArray(int lengtArray, int range) { // создаем метод для создания массива с барьерами
         Random random = new Random();
         int[] arrays = new int[lengtArray];
         for (int i = 0; i < lengtArray; i++) {
-            arrays[i] = random.nextInt(range);
+            arrays[i] = random.nextInt(range +1);
         }
         return arrays;
     }
